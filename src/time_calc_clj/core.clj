b39083a7ec18c9d694f1483c935fd8cd61f8e2c6
Larry Jones <mrwizard82d1@gmail.com>
2014-03-05 10:03:37 -0700

    Wrote line->task. Added record for Task.

--- src/time_calc_clj/core.clj
+++ src/time_calc_clj/core.clj
@@ -1,6 +1,8 @@
 (ns time-calc-clj.core
-  (:use (clojure [string :only [split-lines] :as str]))
+  (:use (clojure [string :only [split, split-lines] :as str]))
   (:gen-class))
+
+(defrecord Task [start end details])
 
 (defn write-summary
   "Write the task summary to *out*."
@@ -11,9 +13,17 @@
   "Summarize a sequence of tasks."
   [task-seq])
 
+(defn line->task
+  "Convert a single line to a task."
+  [a-line]
+  (let [[start details] (str/split a-line #"[ \t]")
+        end start]
+    (Task. start end details)))
+
 (defn lines->tasks
   "Convert a sequences of lines to tasks."
-  [line-seq])
+  [line-seq]
+  (map line->task line-seq))
 
 (defn read-all-lines
   "Read all lines from a file."
