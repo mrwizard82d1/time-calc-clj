79d880208eade2dae4af9d1701713009b1670afe
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2014-03-05 23:14:42 +0000

    Implemented application to summarize (work) task times.

--- src/time_calc_clj/core.clj
+++ src/time_calc_clj/core.clj
@@ -1,22 +1,66 @@
 (ns time-calc-clj.core
   (:use (clojure [string :only [split, split-lines] :as str]))
+  (:import [java.text SimpleDateFormat])
   (:gen-class))
 
 (defrecord Task [start end details])
 
-(defn write-summary
-  "Write the task summary to *out*."
-  [task-summary]
-  (println "The summary."))
+(defrecord TimePoint [hour min])
+
+(defrecord TimeInterval [hours mins])
+
+(defn task-duration
+  "Calcluate the duration of a task."
+  [task]
+  (let [{start-hours :hour start-mins :min} (:start task)
+        {end-hours :hour end-mins :min} (:end task)]
+    (if (< end-mins start-mins)
+      (TimeInterval. (- (- end-hours 1) start-hours)
+                     (- (+ end-mins 60) start-mins))
+      (TimeInterval. (- end-hours start-hours)
+                     (- end-mins start-mins)))))
+
+(defn add-interval
+  "Adds two TimeInteval instances."
+  [interval-1 interval-2]
+  (let [hours-sum (+ (:hours interval-1) (:hours interval-2))
+        minutes-sum (+ (:mins interval-1) (:mins interval-2))]
+    (if (> minutes-sum 59)
+      (TimeInterval. (+ 1 hours-sum) (- minutes-sum 60))
+      (TimeInterval. hours-sum minutes-sum))))
+
+(defn between? 
+  "Is to-test in [lower, upper)?"
+  [to-test lower upper]
+  (and (>= to-test lower) (< to-test upper)))
+
+(defn interval->decimal-interval
+  "Converts a TimeInterval to a decimal value of hours."
+  [{hours :hours mins :mins}]
+  (cond (or (between? mins 0 8) (between? mins 53 60)) hours
+        (between? mins 8 23) (+ hours 0.25)
+        (between? mins 23 38) (+ hours 0.5)
+        (between? mins 39 54) (+ hours 0.75)))
 
 (defn summarize-tasks
   "Summarize a sequence of tasks."
-  [task-seq])
+  [task-seq]
+  (let [interval-map (reduce
+                      #(assoc %1
+                         (:details %2)
+                         (add-interval (task-duration %2)
+                                       (get %1 (:details %2)
+                                            (TimeInterval. 0 0))))
+                         {} task-seq)]
+    (into {} (for [[d i] interval-map]
+               [d (interval->decimal-interval i)]))))
 
 (defn line->task
   "Convert a single line to a task."
   [a-line]
-  (let [[start details] (str/split a-line #"[ \t]")
+  (let [[start-text details] (str/split a-line #"[ \t]")
+        start (TimePoint. (Integer. (subs start-text 0 2))
+                          (Integer. (subs start-text 2)))
         end start]
     (Task. start end details)))
 
@@ -46,7 +90,9 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [filename "time_calc.txt"]
-    (write-summary (summarize-tasks (lines->tasks (read-all-lines
-                                                   filename)))))) 
+  (let [filename "../time_calc/time.txt"]
+    (doseq [[details duration] (summarize-tasks
+                               (lines->tasks (read-all-lines
+                                              filename)))]
+      (println details duration))))
 
