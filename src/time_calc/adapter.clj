939fd12a21c25dfe88df4f42cfcaa38a4fc32ce9
Larry Jones <larry.jones@reveal-energy.com>
2018-03-28 23:52:45 -0500

    Write time-calc.adapter
    - no-empty-lines
    - activity-month-day-text
    - parse-date
    - activities-by-day

--- src/time_calc/adapter.clj
+++ src/time_calc/adapter.clj
@@ -1,11 +1,20 @@
 (ns time-calc.adapter
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [java-time :as jt]))
 
 (defn no-empty-lines [lines]
   (filter not-empty lines))
 
+(defn activity-month-date-text [text]
+  (second (str/split text #"\s")))
+
+(defn parse-date [month-day-text]
+  (jt/month-day "dd-MMM" month-day-text))
+
 (defn activities-by-day [lines]
   (->> lines
+       no-empty-lines
        (partition-by #(re-find #"#\s+(\d{2}-\w{3})" %))
        (partition 2)
-       (map #(vector (second (str/split (first (first %)) #"\s")) (second %)))))
+       (map #(vector (parse-date (activity-month-date-text (first (first %)))) (second %)))
+       (into {} )))
