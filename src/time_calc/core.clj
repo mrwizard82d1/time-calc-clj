c4a77772b3493fecee3e03b447c98fc4f8ee0c56
Larry Jones <larry.jones@reveal-energy.com>
2018-04-03 09:24:00 -0500

    Extracted summarize function

--- src/time_calc/core.clj
+++ src/time_calc/core.clj
@@ -5,14 +5,17 @@
   (:gen-class))
 
 (defn in-filename [args]
-  (let [time-filename (or (first args) "time.md")]
-    (->> time-filename
-         tc.i/all-lines
-         (tc.a.core/lines->activities)
-         (tc.activity/summarize-activities))))
+  (or (first args) "time.md"))
+
+(defn summarize [time-filename]
+  (->> time-filename
+       tc.i/all-lines
+       (tc.a.core/lines->activities)
+       (tc.activity/summarize-activities)))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, time-calc-clj World!")
-  (println (in-filename args)))
+  (println (summarize (in-filename args))))
