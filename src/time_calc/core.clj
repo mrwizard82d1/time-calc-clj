2a985cfec6593a95498fafdc75c9430b56c1667c
Larry Jones <larry.jones@reveal-energy.com>
2018-04-03 09:51:42 -0500

    Formatted summarized output
    Previous commits simply converted the summary data structures to a
    string and wrote that string to the console.
    
    This commit formats the output into summaries grouped by day.

--- src/time_calc/core.clj
+++ src/time_calc/core.clj
@@ -11,11 +11,10 @@
   (->> time-filename
        tc.i/all-lines
        (tc.a.core/lines->activities)
-       (tc.activity/summarize-activities)))
-
+       (tc.activity/summarize-activities)
+       (tc.i/summary-by-day->str)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, time-calc-clj World!")
   (println (summarize (in-filename args))))
--- src/time_calc/io.clj
+++ src/time_calc/io.clj
@@ -5,3 +5,13 @@
   (-> filename
       slurp
       (str/split #"\r?\n")))
+
+(defn summary->str [summary]
+  (str/join "\n"
+            (for [[name duration] summary]
+              (str name ": " duration))))
+
+(defn summary-by-day->str [summary-by-day]
+  (str/join "\n\n" (for [[day summary] summary-by-day]
+                     (str/join "\n" [day (summary->str summary)]))))
+
